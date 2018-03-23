2c5dc3ab9551c00529ef7b8ea6cf74f9313da06d
Renqian Luo <lrqrichard@gmail.com>
2018-03-24 03:57:02 +0800

    work to death

--- train_AmeobaNet_A_6_36.sh
+++ train_AmeobaNet_A_6_36.sh
@@ -1,8 +1,8 @@
-echo Using CUDA $1, train model $2
+MODEL=AmoebaNet_A_6_36
+echo Using CUDA $1, train model $MODEL
 
 export  CUDA_VISIBLE_DEVICES=$1
-#MODEL_DIR=/hdfs/sdrgvc/v-renluo/DAG2N/model/model_$2
-MODEL_DIR=model/model_$2
+MODEL_DIR=model/model_$MODEL
 LOG_DIR=log
 mkdir -p $MODEL_DIR
 mkdir -p $LOG_DIR
@@ -20,4 +20,4 @@ nohup python main_es.py \
   --l_min=0.0 \
   --T_0=600 \
   --dag='AmoebaNet_A' \
-  --lr_schedule=cosine >$LOG_DIR/train.$2.log 2>&1 &
+  --lr_schedule=cosine >$LOG_DIR/train.$MODEL.log 2>&1 &
